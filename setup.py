ca7ff7b9c41d7d1c0fb1bf460e6bca5c2fba797d
CrinitusFeles <crinitusfeles@gmail.com>
2020-02-01 21:22:39 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hypersand',
+      version='1.0',
+      description='Python module control of Rohde&Schwarz HMP4040',
+      url='https://https://github.com/CrinitusFeles/HMP4040',
+      author='deverte',
+      author_email='crinitusfeles@gmail.com',
+      license='MIT',
+      packages=['hmp4040'],
+      zip_safe=False)
