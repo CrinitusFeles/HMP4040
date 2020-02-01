2d0ae875a827bcbc648c31941331da72ff4af6bf
crinitusfeles <crinitusfeles@gmail.com>
2020-02-01 22:47:28 +0700

    init

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='hypersand',
+setup(name='HMP4040',
       version='1.0',
       description='Python module control of Rohde&Schwarz HMP4040',
       url='https://https://github.com/CrinitusFeles/HMP4040',
-      author='deverte',
+      author='crinitusfeles',
       author_email='crinitusfeles@gmail.com',
       license='MIT',
       packages=['hmp4040'],
